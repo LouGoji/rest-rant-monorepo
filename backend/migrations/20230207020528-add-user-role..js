8d8715ebf5054b30488bc255d499fc6f1168bae2
Louis Mantell <loiuismantell@Louis-iMac.local>
2023-02-06 21:45:11 -0500

    final-session

--- backend/migrations/20230207020528-add-user-role..js
+++ backend/migrations/20230207020528-add-user-role..js
@@ -3,16 +3,16 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.addColumn('users', 'role', {
-      type: Sequelize.DataTypes.ENUM,
-      values: [
-        'reviewer',
-        'admin',
-      ],
-      defaultValue: 'reviewer'
-    })
+        type: Sequelize.ENUM,
+        values: [
+          'reviewer',
+          'admin',
+        ],
+        defaultValue: 'reviewer'
+      })
   },
 
   down: async (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('users', 'role')
   }
-}
+};
